23467f16fa3417d40bf61f4e25c771de11788ad9
Kinetic Napier <u61819269@gmail.com>
2026-09-21 22:52:24 +0900

    Load Java content batch 007

--- src/content-loader.js
+++ src/content-loader.js
@@ -7,7 +7,8 @@ export const CONTENT_CATEGORIES = [
   'java-expansion-003.json',
   'java-expansion-004.json',
   'java-expansion-005.json',
-  'java-expansion-006.json'
+  'java-expansion-006.json',
+  'java-expansion-007.json'
 ];
 
 export async function loadLocalizedContent({ fetchJson, locale, fallbackLocale = 'ja' }) {
