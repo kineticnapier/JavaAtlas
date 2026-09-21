5efd09a37194b062b7455b5447c01e4f4a0cd514
Kinetic Napier <u61819269@gmail.com>
2026-09-21 19:51:39 +0900

    Update corpus shard expectation for 150 articles

--- test/template-foundation.test.js
+++ test/template-foundation.test.js
@@ -4,7 +4,7 @@ import { CONTENT_CATEGORIES } from '../src/content-loader.js';
 import { matchesArticle } from '../src/article-search.js';
 
 test('JavaAtlas loads the Java content corpus shards', () => {
-  assert.deepEqual(CONTENT_CATEGORIES, ['java-core.json', 'java-expansion-001.json', 'java-expansion-002.json', 'java-expansion-003.json', 'java-expansion-004.json', 'java-expansion-005.json']);
+  assert.deepEqual(CONTENT_CATEGORIES, ['java-core.json', 'java-expansion-001.json', 'java-expansion-002.json', 'java-expansion-003.json', 'java-expansion-004.json', 'java-expansion-005.json', 'java-expansion-006.json']);
 });
 
 test('article search remains reusable', () => {
