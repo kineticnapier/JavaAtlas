f39e4aefbe865f6083c622d663a1e790dfcb4d84
Kinetic Napier <u61819269@gmail.com>
2026-09-21 15:53:19 +0900

    content: load third Java expansion batch

--- src/content-loader.js
+++ src/content-loader.js
@@ -3,7 +3,8 @@ import { localizeArticles } from './article-localization.js';
 export const CONTENT_CATEGORIES = [
   'java-core.json',
   'java-expansion-001.json',
-  'java-expansion-002.json'
+  'java-expansion-002.json',
+  'java-expansion-003.json'
 ];
 
 export async function loadLocalizedContent({ fetchJson, locale, fallbackLocale = 'ja' }) {
