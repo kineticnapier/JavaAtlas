b2e5d9bc881f8dfdc7a9d238191a3f3eee4e3c75
Kinetic Napier <u61819269@gmail.com>
2026-09-22 12:55:27 +0900

    test: require 200 Java articles

--- test/java-content-150.test.js
+++ test/java-content-150.test.js
@@ -7,12 +7,12 @@ async function readJson(path) {
   return JSON.parse(await readFile(new URL(path, import.meta.url), 'utf8'));
 }
 
-test('JavaAtlas next content batch brings the corpus to 190 articles', async () => {
-  assert.ok(CONTENT_CATEGORIES.includes('java-expansion-010.json'));
+test('JavaAtlas next content batch brings the corpus to 200 articles', async () => {
+  assert.ok(CONTENT_CATEGORIES.includes('java-expansion-011.json'));
   let count = 0;
   for (const file of CONTENT_CATEGORIES) {
     const articles = await readJson(`../public/content/articles/${file}`);
     count += articles.length;
   }
-  assert.equal(count, 190);
+  assert.equal(count, 200);
 });
