7cdb875e14f50f62304f2a45576a40fd51ff8516
Kinetic Napier <u61819269@gmail.com>
2026-09-23 00:50:04 +0900

    test: require Java content expansion 023

--- test/java-content-150.test.js
+++ test/java-content-150.test.js
@@ -7,12 +7,12 @@ async function readJson(path) {
   return JSON.parse(await readFile(new URL(path, import.meta.url), 'utf8'));
 }
 
-test('JavaAtlas next content batch brings the corpus to 310 articles', async () => {
-  assert.ok(CONTENT_CATEGORIES.includes('java-expansion-022.json'));
+test('JavaAtlas next content batch brings the corpus to 320 articles', async () => {
+  assert.ok(CONTENT_CATEGORIES.includes('java-expansion-023.json'));
   let count = 0;
   for (const file of CONTENT_CATEGORIES) {
     const articles = await readJson(`../public/content/articles/${file}`);
     count += articles.length;
   }
-  assert.equal(count, 310);
+  assert.equal(count, 320);
 });
