8de108302e23da6c4cd7c3872937fbcf241b4ae2
Kinetic Napier <u61819269@gmail.com>
2026-09-22 22:56:33 +0900

    content: register Java expansion 021

--- src/content-loader.js
+++ src/content-loader.js
@@ -21,7 +21,8 @@ export const CONTENT_CATEGORIES = [
   'java-expansion-017.json',
   'java-expansion-018.json',
   'java-expansion-019.json',
-  'java-expansion-020.json'
+  'java-expansion-020.json',
+  'java-expansion-021.json'
 ];
 
 export async function loadLocalizedContent({ fetchJson, locale, fallbackLocale = 'ja' }) {
