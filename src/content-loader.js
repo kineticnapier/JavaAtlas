0dbb4cb9e6a0c77722922476fcd8aef7d7a347b5
Kinetic Napier <u61819269@gmail.com>
2026-09-23 21:53:20 +0900

    Load Java expansion batch 039

--- src/content-loader.js
+++ src/content-loader.js
@@ -39,7 +39,8 @@ export const CONTENT_CATEGORIES = [
   'java-expansion-035.json',
   'java-expansion-036.json',
   'java-expansion-037.json',
-  'java-expansion-038.json'
+  'java-expansion-038.json',
+  'java-expansion-039.json'
 ];
 
 export async function loadLocalizedContent({ fetchJson, locale, fallbackLocale = 'ja' }) {
